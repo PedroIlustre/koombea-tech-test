03fa5d7055b7b1186b71f53cfeedbad3f268861e
Pedro Ilustre <pedro.ilustre.ext@infracommerce.com.br>
2021-09-04 22:50:25 -0300

    feat: add .csv test file and sql file

--- README.md
+++ README.md
@@ -4,7 +4,9 @@ PHPunit to run some testes and the 7.4.13 version of PHP
 
 To start this project, the user has to run composer install to get all the dependencies that this projetc is using.
 
-To prepare the database it needed to run de tech-test.sql file at one database, and after that it needed to run the 
+To prepare the database it needed to run de create_schema_table.sql file at one database (this one can be found at the root directory of this project), and after that it needed to run the 
 comand php artisan migrate:refresh to create all the tables that this application will use.
 
-checkout the .env file to set the right port or right host from where your database is set
+checkout the .env file to set the right port or right host from where your database is set.
+
+Some .csv file can be found at the root directory of this project.
---
+++ create_schema_table.sql
@@ -0,0 +1,35 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 05-Set-2021 às 03:36
+-- Versão do servidor: 10.4.21-MariaDB
+-- versão do PHP: 8.0.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `tech-test`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `create_schema_table`
+--
+-- Erro ao ler a estrutura para a tabela tech-test.create_schema_table: #1146 - Tabela 'tech-test.create_schema_table' não existe
+-- Erro ao ler dados para tabela tech-test.create_schema_table: #1064 - Você tem um erro de sintaxe no seu SQL próximo a 'FROM `tech-test`.`create_schema_table`' na linha 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
