89ef4dea3f8d4a1963a6fa523eaf6399d4e0b2c0
Pedro Ilustre <pedro.ilustre.ext@infracommerce.com.br>
2021-09-04 22:45:58 -0300

    feat: fix some bug

--- app/Http/Controllers/UploadContactFileController.php
+++ app/Http/Controllers/UploadContactFileController.php
@@ -9,7 +9,7 @@ use App\Http\Controllers\ContactController;
 
 class UploadContactFileController extends Controller
 {
-    public function uploadFile ($request) 
+    public function uploadFile (Request $request) 
     {
         $file = $request->file('file_uploaded');
 
